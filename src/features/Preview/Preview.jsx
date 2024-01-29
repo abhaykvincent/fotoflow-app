a2ab20f5be3b71e121234d94400153fbbddac2f9
Abhay <abhaykvincent@gmail.com>
2024-01-29 23:48:01 +0530

    Add image preview controls

--- src/features/Preview/Preview.jsx
+++ src/features/Preview/Preview.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Preview.scss'
+import { shortenFileName } from '../../utils/stringUtils'
 
 function Preview({image,setPreviewIndex}) {
   console.log(image)
@@ -7,9 +8,11 @@ function Preview({image,setPreviewIndex}) {
     <div className='preview'>
         <div className="image-wrap">
             <div className="image"
-            style={{ backgroundImage: `url(${image})` }}
+            style={{ backgroundImage: `url(${image.url})` }}
             > </div>
-            <div className="controls">
+            
+        </div>
+        <div className="controls">
               <div className="next"
               onClick={()=>setPreviewIndex(prevIndex=>prevIndex+1)}
               ></div>
@@ -17,7 +20,23 @@ function Preview({image,setPreviewIndex}) {
               onClick={()=>setPreviewIndex(prevIndex=>prevIndex-1)}
               ></div>
             </div>
-        </div>
+            <div className="controls top">
+              <div className="left-controls">
+                <div className="back"></div>
+                <div className="file-name">{shortenFileName(image.name)}</div>
+              </div>
+              <div className="right-controls">
+                <div className="icon download"></div>
+                <div className="icon share"></div>
+              </div>
+              <div className="center-controls">
+                <div className="magnifier">
+                  <div className="zoom-out"></div>
+                  <div className="zoom-value">50%</div>
+                  <div className="zoom-in"></div>
+                </div>
+              </div>
+            </div>
     </div>
   )
 }
